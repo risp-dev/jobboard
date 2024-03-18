569f2fa0533537275f8b7e42854749cf768bb9e6
Rimas <119005269+risp-dev@users.noreply.github.com>
2024-03-18 22:26:33 +0200

    Login form done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
+
+<section class="section-hero overlay inner-page bg-image" style="background-image: url('{{ asset('assets/images/hero_1.jpg') }}');" id="home-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <h1 class="text-white font-weight-bold">Log In</h1>
+          <div class="custom-breadcrumbs">
+            <a href="#">Home</a> <span class="mx-2 slash">/</span>
+            <span class="text-white"><strong>Log In</strong></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+        <div class="col-md-12">
+
+            <form action="{{ route('login')}}" method="POST" class="p-4 border rounded mt-4 mb-4">
+                @csrf
+            
+                <div class="row form-group">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                        <label class="text-black" for="fname">Email</label>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                       
+                       
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+            
+                <div class="row form-group mb-4">
+                    <div class="col-md-12 mb-3 mb-md-0">
+                        <label class="text-black" for="fname">Password</label>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                    </div>
+                </div>
+            
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <input type="submit" name="submit" value="Log In" class="btn px-4 btn-primary text-white">
+                    </div>
+                </div>
+            </form>
+            
+
+
+            {{-- <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -66,7 +115,7 @@
                         </div>
                     </form>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
